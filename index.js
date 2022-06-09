bc73656fbd1ddd76b2f7eeb1270f2c2d1bdb8dcb
purity-wangu <purity.kariuki@student.moringaschool.com>
2022-06-09 13:53:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+//generates a random integer greater than 0
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+
+let max =Math.max(3, 7, 13, 17, 20);
+
+
+
